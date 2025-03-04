7ed9b1774b1f9e954a83d855f40eccd485dd8279
dan1e-dan <deswz97@gmail.com>
2025-03-05 01:13:14 +0800

    Update next.config.ts
    change rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }
+   /* config options here */
+
 
 export default nextConfig;
